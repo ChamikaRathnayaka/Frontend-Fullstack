1152995626d06fb865320ed1db57529dc57a38a4
ChamikaRathnayaka <chamikarath97@gmail.com>
2023-05-19 12:31:05 +0530

    Got data from backend and create add user page

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -22,7 +23,7 @@ export default function Navbar() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
           </div>
-          <button className="btn btn-outline-light" >Add User</button>
+          <Link className="btn btn-outline-light" to="/adduser" >Add User</Link>
         </div>
         
       </nav>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,18 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-export default function Home () {
+export default function Home() {
+  const [users, setUsers] = useState([]);
 
-    const [users, setUsers] = useState([]);
+  useEffect(() => {
+    loadUsers();
+  }, []);
 
-    useEffect(() =>{
-        loadUsers();
-    },[]);
-
-  const loadUsers=async () =>{
-    const result=await axios.get("http://localhost:8090/users");
-    console.log(result)
-    setUsers(result.data)
-    
+  const loadUsers = async () => {
+    const result = await axios.get("http://localhost:8090/users");
+    console.log(result);
+    setUsers(result.data);
   };
- 
 
   return (
     <div className="container">
@@ -23,29 +20,35 @@ export default function Home () {
         <table class="table border shadow">
           <thead>
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th scope="col">Index</th>
+              <th scope="col">Name</th>
+              <th scope="col">User Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
-
-            {
-                
-                users.map((user, index)=>
-                (
-                    <tr>
-                    <th scope="row" key={index}>{index+1}</th>
-                    <td>{user.name}</td>
-                    <td>{user.username}</td>
-                    <td>{user.email}</td>
-                  </tr>
-                ))
-            }
-
-           
-            
+            {users.map((user, index) => (
+              <tr>
+                <th scope="row" key={index}>
+                  {index + 1}
+                </th>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>
+                  <button type="button" class="btn btn-primary mx-2">
+                    View
+                  </button>
+                  <button type="button" class="btn btn-success mx-2">
+                    Edit
+                  </button>
+                  <button type="button" class="btn btn-danger mx-2">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
