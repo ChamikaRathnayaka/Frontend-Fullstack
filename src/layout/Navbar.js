8ce96878a38b923ae901825ad4be9e3587dea7ee
ChamikaRathnayaka <chamikarath97@gmail.com>
2023-05-19 11:21:00 +0530

    connected frontend to the backend using axios

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            Navbar
+            USER MANAGEMENT SYSTEM
           </a>
           <button
             className="navbar-toggler"
@@ -20,51 +20,11 @@ export default function Navbar() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
-              </li>
-            </ul>
+
           </div>
+          <button className="btn btn-outline-light" >Add User</button>
         </div>
+        
       </nav>
     </div>
   );
