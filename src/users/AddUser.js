fc5e1d82fe792714928798e3095f7c205630138c
ChamikaRathnayaka <chamikarath97@gmail.com>
2023-05-22 16:05:09 +0530

    deleting modal created

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -30,11 +30,11 @@ export default function AddUser() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow  " >
           <h2 className="text-center m-4">Register User</h2>
 
           <form onSubmit={(e) => onSubmit(e)}>
-            <div className="mb-3">
+            <div className="mb-3  ">
               <label htmlFor="Name" className="form-label">
                 Name
               </label>
